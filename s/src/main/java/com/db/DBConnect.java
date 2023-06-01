ea1f7840cac1370d771e440833d02c0e14df757a
saurabhhigh1999 <127730433+saurabhhigh1999@users.noreply.github.com>
2023-06-01 17:16:26 +0530

    Add files via upload

---
+++ s/src/main/java/com/db/DBConnect.java
@@ -0,0 +1,24 @@
+package com.db;
+
+//package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	private static Connection conn;
+
+	public static Connection getConn() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/hospital_2", "root", "root");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+}
